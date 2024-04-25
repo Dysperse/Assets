d40f6cf0ce705c12287c8b023a8270411816af2c
Manu G <manuthecoder@protonmail.com>
2024-04-25 11:28:19 -0700

    Update drum.js

--- hack-club/drum.js
+++ hack-club/drum.js
@@ -1,3 +1,4 @@
 const sounds = ['SD0025', 'SD0010', 'SD0000', 'RS', 'OH25', 'MA', 'CY0010', 'CH', 'CB', 'BD0010', 'BD0000', 'CP'];
 const base = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518"
 const getURL = (i) => `${base}/${sounds[i]}.mp3`;
+window.getURL = getURL
